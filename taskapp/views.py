e2dbf21a768c3327e39f50361d86b75935fb26f1
carlosmf996 <carlosmf996@gmail.com>
2023-11-08 10:19:33 +0100

    Añado formulario interno en el HTML

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,10 +1,19 @@
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
+
+# Create your views here.
 
 def task_list(request):
 
     task = Task.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'tasks/task_list.html', {'task': task})
 
-# Create your views here.
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form= TaskForm()
+    return render(request, 'tasks/task_list.html', {'task': task, 'form': form})
